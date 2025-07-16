1f7b934775f6cb042d8321a84f7888d207ad5dfd
Layrane Leão <167014394+LayraneLeao@users.noreply.github.com>
2025-07-15 21:47:17 -0300

    Create traducao-meses-para-ingles.py

---
+++ traducao-meses-para-ingles.py
@@ -0,0 +1,28 @@
+mes = input("Digite o nome do mês: ").lower()
+
+if mes == "janeiro":
+  print("January")
+elif mes == "fevereiro":
+  print("February")
+elif mes == "março":
+  print("March")
+elif mes == "abril":
+  print("April")
+elif mes == "maio":
+  print("May")
+elif mes == "junho":
+  print("June") 
+elif mes == "julho":
+  print("July") 
+elif mes == "agosto":
+  print("August")
+elif mes == "setembro":
+  print("September")
+elif mes == "outubro":
+  print("October")
+elif mes == "novembro":
+  print("November")
+elif mes == "dezembro":
+  print("December")
+else:
+  print("Operação Inválida!")
